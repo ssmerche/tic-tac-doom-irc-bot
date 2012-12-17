d95ac413302edbbff21e92906073a1896c182915
Scott Smerchek <ssmerche@gmail.com>
2012-12-16 22:54:17 -0800

    use lein 2

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
                  [compojure "1.1.3"]]
   :plugins [[lein-ring "0.7.5"]]
   :ring {:handler compojure-test.handler/app}
+  :min-lein-version "2.0.0"
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}})
