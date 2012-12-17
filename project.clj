ca10f369ea346c737066dfea75accc1393e2d7a1
Scott Smerchek <ssmerche@gmail.com>
2012-12-16 22:47:43 -0800

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject compojure-test "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.3"]]
+  :plugins [[lein-ring "0.7.5"]]
+  :ring {:handler compojure-test.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
