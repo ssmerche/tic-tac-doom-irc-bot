a9f4f84a484520dab814ebff11cc33959130fb76
Scott Smerchek <ssmerche@gmail.com>
2012-12-20 01:34:47 -0800

    basic version of tic-tac-doom bot

--- project.clj
+++ project.clj
@@ -2,9 +2,17 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.3"]]
+                 [compojure "1.1.3"]
+                 [cc.qbits/ash "0.2.4"]
+                 [korma "0.3.0-beta11"]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]
+                 [log4j "1.2.15" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]]
   :plugins [[lein-ring "0.7.5"]]
-  :ring {:handler compojure-test.handler/app}
+  :ring {:handler compojure-test.handler/app
+         :init compojure-test.handler/start-bot}
   :min-lein-version "2.0.0"
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}})
--- src/compojure_test/handler.clj
+++ src/compojure_test/handler.clj
@@ -1,11 +1,52 @@
 (ns compojure-test.handler
-  (:use compojure.core)
+  (:use compojure.core korma.db korma.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [qbits.ash :as ash]
+            [clojure.string :as string])
+  (:import [java.sql Date] [java.net URI]))
+
+(defn db-info-from-url [url]
+  (if (empty? url)
+    {:host "localhost" :port 5432 :user (System/getenv "USER") :db "tic_tac_doom_irc"}
+    (let [uri (URI. url)
+          host (.getHost uri)
+          port (.getPort uri)
+          [user password] ((string/split (.getUserInfo uri) #":"))
+          db (.substring (.getPath uri) 1)]
+      {:host host :port port :user user :password password :db db})))
+
+(defdb db (postgres (db-info-from-url (System/getenv "HEROKU_POSTGRESQL_IVORY_URL"))))
+
+(defentity messages)
+
+(def room (or (System/getenv "BOT_ROOM") "#tic-tac-doom"))
+
+(def bot-name (or (System/getenv "BOT_NAME") "tic-tac-doom-bot"))
+
+(def irc-info [:nick bot-name :name bot-name :port 6667
+               :channels [room] :host "irc.freenode.net"])
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (pr-str (select messages)))
   (route/not-found "Not Found"))
+
+; rename user key to user since postgres won't let me have a column called user
+(defn tweak-event [event]
+  (-> event (assoc :username (event :user)) (dissoc :user)
+    (update-in [:timestamp] #(Date. %))))
+
+(defn record-event [event]
+  (insert messages (values event)))
 
 (def app
   (handler/site app-routes))
+
+; event has keys user, channel, content, timestamp
+(defn bot-handler [bot]
+  (ash/listen bot :on-message 
+              (fn [event] (record-event (tweak-event event)))))
+
+(defn start-bot [& args]
+  (let [bot (-> (apply ash/make-bot irc-info) bot-handler)]
+    (ash/send-message bot room "Hello from the tic-tac-doom bot")))
