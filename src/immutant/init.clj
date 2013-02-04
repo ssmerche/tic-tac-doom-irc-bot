4b575a5586e1225f3072961d32ec7ff2c10bf2ab
Scott Smerchek <ssmerche@gmail.com>
2013-02-04 13:48:08 -0800

    create immutant daemon for irc bot

--- src/immutant/init.clj
+++ src/immutant/init.clj
@@ -1,6 +1,8 @@
 (ns immutant.init
   (:require [tic-tac-doom-bot.app :as app]
-            [immutant.messaging :as messaging]
+            [tic-tac-doom-bot.bot :as bot]
+            [immutant.daemons :as daemon]
             [immutant.web :as web]))
 
+(daemon/daemonize "tic-tac-doom-bot" bot/start-bot bot/stop-bot :singleton false)
 (web/start app/app)
--- src/tic_tac_doom_bot/bot.clj
+++ src/tic_tac_doom_bot/bot.clj
@@ -2,14 +2,22 @@
   (:require [qbits.ash :as ash])
   (:use tic-tac-doom-bot.plugins environ.core))
 
+(defonce bot (atom nil))
+
 (def bot-name (env :bot-name))
 
 (def room (env :bot-room))
 
 (def irc-info [:nick bot-name :name bot-name :port 6667
                :channels [room] :host "irc.freenode.net"])
+
 (defn greet-room [bot msg] (ash/send-message bot room msg))
 
-(defn start-bot [& args]
-  (let [bot (-> (apply ash/make-bot irc-info) record-messages)] 
-    (greet-room bot "Hello from the tic-tac-doom bot")))
+(defn start-bot []
+  (let [new-bot (-> (apply ash/make-bot irc-info) record-messages)] 
+    (greet-room new-bot "Hello from the tic-tac-doom bot")
+    (reset! bot new-bot)))
+
+(defn stop-bot []
+  (ash/shutdown @bot)
+  (reset! bot nil))
